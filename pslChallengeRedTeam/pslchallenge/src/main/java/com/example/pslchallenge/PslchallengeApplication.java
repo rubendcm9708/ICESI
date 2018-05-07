114725c65258cbaf4037e4917b075b3308dc8fa6
Andres Zapata <azo_67@hotmail.es>
2018-05-07 16:02:28 -0500

    Printing Hello World in port 8080
    Andres Zapata
    Johann Yaqueno
    Eileen Guerrero

---
+++ pslChallengeRedTeam/pslchallenge/src/main/java/com/example/pslchallenge/PslchallengeApplication.java
@@ -0,0 +1,12 @@
+package com.example.pslchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PslchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PslchallengeApplication.class, args);
+	}
+}
