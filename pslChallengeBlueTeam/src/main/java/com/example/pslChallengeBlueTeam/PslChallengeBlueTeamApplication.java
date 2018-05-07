16e2267413d9e507ac0b5305dac56ffb7ee1afef
unknown <garzuxo@gmail.com>
2018-05-07 12:49:08 -0700

    se agrega carpeta spring

---
+++ pslChallengeBlueTeam/src/main/java/com/example/pslChallengeBlueTeam/PslChallengeBlueTeamApplication.java
@@ -0,0 +1,12 @@
+package com.example.pslChallengeBlueTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PslChallengeBlueTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PslChallengeBlueTeamApplication.class, args);
+	}
+}
